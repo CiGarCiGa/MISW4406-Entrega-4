1684b6e9ff1155c9066f631458afa9fe4dbbc800
maridelvalle15 <marisela.delvalle93@gmail.com>
2023-03-04 23:55:21 -0500

    agregado entidades eventos y obj valor

---
+++ gestorcompras/src/modulos/inventario/dominio/entidades.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+
+import src.modulos.inventario.dominio.objetos_valor as ov
+from src.modulos.inventario.dominio.eventos import InventarioValidado
+from src.seedwork.dominio.entidades import AgregacionRaiz
+
+@dataclass
+class Orden(AgregacionRaiz):
+    id_cliente: uuid.UUID = field(hash=True, default=None)
+    productos: list[ov.ProductoOrden] = field(default_factory=list[ov.ProductoOrden])
+
+    def validar_inventario(self, orden: Orden):
+        self.id_cliente = orden.id_cliente
+        self.productos = orden.estado
+
+        self.agregar_evento(InventarioValidado(id_orden=self.id, id_cliente=self.id_cliente))
