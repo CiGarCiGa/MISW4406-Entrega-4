ebeea88748c1f2ac1747b3099496646e3b66756c
maridelvalle15 <marisela.delvalle93@gmail.com>
2023-03-04 21:12:47 -0500

    dto para orden y validar inventario

---
+++ gestorcompras/src/modulos/inventario/aplicacion/dto.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from src.seedwork.aplicacion.dto import DTO
+
+@dataclass(frozen=True)
+class ProductoDTO(DTO):
+    sku: str
+    cantidad: str
+
+@dataclass(frozen=True)
+class OrdenDTO(DTO):
+    fecha_creacion: str = field(default_factory=str)
+    fecha_actualizacion: str = field(default_factory=str)
+    id: str = field(default_factory=str)
+    productos: list[ProductoDTO] = field(default_factory=list)
