23babb56506d028ea98544a86d489f813a5013a8
maridelvalle15 <marisela.delvalle93@gmail.com>
2023-03-10 00:10:17 -0500

    ajuste orden tiene lista de productos, se envia lista directo en string por mensaje

--- consolidador/src/modulos/inventario/aplicacion/comandos/validar_inventario.py
+++ consolidador/src/modulos/inventario/aplicacion/comandos/validar_inventario.py
@@ -10,7 +10,7 @@ class ValidarInventario(Comando):
     fecha_creacion: str
     fecha_actualizacion: str
     id: str
-    productos_orden: OrdenDTO
+    productos_orden: str
 
 
 class ValidarInventarioHandler():
