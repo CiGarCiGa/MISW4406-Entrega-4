e6e0c30ab6dca44c340aab1655ce2689ba94d760
maridelvalle15 <marisela.delvalle93@gmail.com>
2023-03-09 21:04:32 -0500

    ajuste validar inventario

--- gestor-compra/src/modulos/inventario/aplicacion/comandos/validar_inventario.py
+++ gestor-compra/src/modulos/inventario/aplicacion/comandos/validar_inventario.py
@@ -13,7 +13,7 @@ class ValidarInventario(Comando):
     fecha_creacion: str
     fecha_actualizacion: str
     id: str
-    productos_orden: list[OrdenDTO]
+    productos_orden: OrdenDTO
 
 
 class ValidarInventarioHandler():
