e1d38d1c5d59d86c6a46873595628cca67ca9b92
CiGarCiGa <ci.garcia81@uniandes.edu.co>
2023-03-14 14:50:55 -0500

    Fix configguration

--- bff_web/src/main.py
+++ bff_web/src/main.py
@@ -32,14 +32,19 @@ eventos = list()
 async def app_startup():
     global tasks
     global eventos
-    task1 = asyncio.ensure_future(suscribirse_a_topico("eventos-compra", "aeroalpes-bff", "public/default/eventos-compra", eventos=eventos))
-    tasks.append(task1)
-
+    #task1 = asyncio.ensure_future(suscribirse_a_topico("eventos-compra", "aeroalpes-bff", "public/default/eventos-compra", eventos=eventos))
+    #tasks.append(task1)
+"""
 @app.on_event("shutdown")
 def shutdown_event():
     global tasks
     for task in tasks:
         task.cancel()
+
+@app.get('/heath')
+def health():
+    return {"health":"ok"}
+
 
 @app.get('/stream')
 async def stream_mensajes(request: Request):
@@ -59,6 +64,6 @@ async def stream_mensajes(request: Request):
             await asyncio.sleep(0.1)
 
     return EventSourceResponse(leer_eventos())
-
+"""
 
 app.include_router(v1, prefix="/v1")
